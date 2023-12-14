d2740f7c207e712b300f040db3650ae718b327bb
dm-zhuk <140021292+dm-zhuk@users.noreply.github.com>
2023-12-14 23:57:55 +0200

    task-01.js update

--- js/task-01.js
+++ js/task-01.js
@@ -3,25 +3,25 @@
 2. Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
 Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.  */
 
-const items = document.querySelectorAll("#categories .item");
-console.log("Number of categories:", items.length); // Number of categories: 3 //
+const list = document.querySelector("#categories");
+const itemElements = document.querySelectorAll(".item");
+console.log(`Number of categories: ${itemElements.length}`); // Number of categories: 3
 
-items.forEach((item) => {
-  let title = item.querySelector("h2").textContent;
-  let elements = item.querySelectorAll("li").length;
-  console.log("Category:", title);
-  console.log("Elements:", elements);
+const firstItemElement = list.firstElementChild;
+const lastItemElement = list.lastElementChild;
+const listItemElements = Array.from(list.children);
+
+listItemElements.forEach((item) => {
+  const title = item.querySelector("h2");
+  const itemsList = item.querySelectorAll("ul li");
+
+  console.log(`Category: ${title.textContent}`);
+  console.log(`Number of elements: ${itemsList.length}`);
 });
 
 // Category: Animals
-// Elements: 4
-
+// Number of elements: 4
 // Category: Products
-// Elements: 3
-
+// Number of elements: 3
 // Category: Technologies
-// Elements: 5
-
-// calcNumberOfCategories;
-
-// const items = document.getElementsByClassName("item");
+// Number of elements: 5
